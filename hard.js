1a39e57874068b779a2a936dbad89954bfcb7450
Abraham Varguez <abraham@Administrators-MacBook-Pro.local>
2023-06-05 16:24:36 -0400

    yo

---
+++ hard.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const employees = require("./employees.json");
+
+// GET endpoint for all employees
+app.get("/employee", (req, res) => {
+  res.json(employee);
+});
+
+// GET endpoint for a specific employee
+app.get("/employees/:employeeID", (req, res) => {
+  const { employeeID } = req.params;
+  const employee = employees.employees.find(
+    (emp) => emp.employeeID === parseInt(employeeID)
+  );
+
+  if (employee) {
+    res.json(employee);
+  } else {
+    res.status(404).json({ error: "Employee not found" });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
